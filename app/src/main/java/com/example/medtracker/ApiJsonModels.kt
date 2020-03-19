8070370551a59ac30da8dc37791ab4626f8f2113
FinnLiersch <54947828+FinnLiersch@users.noreply.github.com>
2020-03-19 14:50:29 +0100

    colors, back button,

---
+++ app/src/main/java/com/example/medtracker/ApiJsonModels.kt
@@ -0,0 +1,6 @@
+package com.example.medtracker
+
+
+class HomeFeed (val videos: List<Video>)
+
+class Video (val id: Int, val name: String, val link: String, val imageUrl: String, val numberOfViews: Int)
