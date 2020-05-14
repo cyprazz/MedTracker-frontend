cab4a28d1ff23edbdc36234a4f1a62eb10161804
FinnLiersch <54947828+FinnLiersch@users.noreply.github.com>
2020-05-15 01:17:42 +0200

    tabs in clander

--- app/src/main/java/com/example/medtracker/Calender.kt
+++ app/src/main/java/com/example/medtracker/Calender.kt
@@ -5,34 +5,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.fragment_calender.*
-import kotlinx.android.synthetic.main.substance_search.*
 
 class Calender : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
         inflater.inflate(R.layout.fragment_calender, container, false)
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        ButtonDay.setOnClickListener{
-            ButtonDay.setBackgroundResource(R.drawable.button_background_selected)
-            ButtonWeek.setBackgroundResource(R.drawable.button_background)
-            ButtonMonth.setBackgroundResource(R.drawable.button_background)
-        }
-        ButtonWeek.setOnClickListener{
-            ButtonDay.setBackgroundResource(R.drawable.button_background)
-            ButtonWeek.setBackgroundResource(R.drawable.button_background_selected)
-            ButtonMonth.setBackgroundResource(R.drawable.button_background)
-            
-        }
 
-        ButtonMonth.setOnClickListener{
-            ButtonDay.setBackgroundResource(R.drawable.button_background)
-            ButtonWeek.setBackgroundResource(R.drawable.button_background)
-            ButtonMonth.setBackgroundResource(R.drawable.button_background_selected)
-        }
+        val fragmentAdapter = fragmentManager?.let { CalenderAdapter(it) }
+        viewPager.adapter = fragmentAdapter
+
+        tabLayout.setupWithViewPager(viewPager)
 
     }
 
-
 }
+
+
+
+
+//ButtonDay.setOnClickListener{
+//    ButtonDay.setBackgroundResource(R.drawable.button_background_selected)
+//    ButtonWeek.setBackgroundResource(R.drawable.button_background)
+//    ButtonMonth.setBackgroundResource(R.drawable.button_background)
+//}
+//ButtonWeek.setOnClickListener{
+//    ButtonDay.setBackgroundResource(R.drawable.button_background)
+//    ButtonWeek.setBackgroundResource(R.drawable.button_background_selected)
+//    ButtonMonth.setBackgroundResource(R.drawable.button_background)
+//
+//}
+//
+//ButtonMonth.setOnClickListener{
+//    ButtonDay.setBackgroundResource(R.drawable.button_background)
+//    ButtonWeek.setBackgroundResource(R.drawable.button_background)
+//    ButtonMonth.setBackgroundResource(R.drawable.button_background_selected)
+//}
---
+++ app/src/main/java/com/example/medtracker/CalenderAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.medtracker
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentPagerAdapter
+
+import androidx.fragment.app.FragmentManager
+
+class CalenderAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                Tab1Day()
+            }
+            1 -> Tab2Week()
+            else -> {
+                return Tab3Month()
+            }
+        }
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return when (position) {
+            0 -> "Day"
+            1 -> "Week"
+            else -> {
+                return "Maand"
+            }
+        }
+    }
+}
